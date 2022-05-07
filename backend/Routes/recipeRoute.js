e7c3398dcaa60c88f1cb6d8497d029668605a7d2
kaimue <92952727+kaimue@users.noreply.github.com>
2022-05-07 17:01:01 +0200

    updated backend with cors

--- backend/Routes/recipeRoute.js
+++ backend/Routes/recipeRoute.js
@@ -3,14 +3,11 @@ import recipe from "../models/recipeSchema.js";
 const router = express.Router();
 
 router.get("/recipes/:name", (req, res) => {
-  recipe.find({}, (error, data) => {
+  recipe.find({ name: req.params.name }, (error, data) => {
     if (error) {
       res.status(400).send("no recipe found");
     }
-    //console.log(req);
-    const singleRecipe = data.find((recipe) => recipe.name === req.params.name);
-    console.log(singleRecipe);
-    res.send(singleRecipe); //
+    res.send(data);
   });
 });
 
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import express from "express";
 import recipeSchema from "./models/recipeSchema.js";
 import recipesRoute from "./Routes/recipesRoute.js";
 import recipeRoute from "./Routes/recipeRoute.js";
+import cors from "cors";
+
 const app = express();
 
 const connection = `mongodb+srv://panaka:${process.env.PW}@cluster0.sacs6.mongodb.net/recipe?retryWrites=true&w=majority`;
@@ -14,6 +16,7 @@ mongoose.connection.on(
   console.error.bind(console, "connection error")
 );
 
+app.use(cors());
 app.use(express.json());
 app.use("/", recipesRoute);
 app.use("/", recipeRoute);
